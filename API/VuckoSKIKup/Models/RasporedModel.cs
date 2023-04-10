593c0afafc7d2f916858ba7482c280f96d491aa3
Alen Lović <alenlovic1987@gmail.com>
2023-04-10 16:03:25 +0200

    changes on application

---
+++ API/VuckoSKIKup/Models/RasporedModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VuckoSKIKup.Models
+{
+    public class RasporedModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ImeTakmičara { get; set; }
+        public string PrezimeTakmičara { get; set; }
+        public int RedniBroj { get; set; }
+        public string NacionalnostTakmičara { get; set; }
+        public string Disciplina { get; set; }
+        public string Staza { get; set; }
+        public string VrijemePolaska { get; set; }
+    }
+}

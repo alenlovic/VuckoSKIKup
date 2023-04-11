833e90c3a1cdcecd679b9070c3d1ab116a948c33
Alen Lović <alenlovic1987@gmail.com>
2023-04-11 19:12:21 +0200

    changed names of tables in EF

---
+++ API/VuckoSKIKup/Migrations/20230411164258_names.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VuckoSKIKup.Migrations
+{
+    /// <inheritdoc />
+    public partial class names : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Staza",
+                table: "Raspored",
+                newName: "StazaPrijava");
+
+            migrationBuilder.RenameColumn(
+                name: "RedniBroj",
+                table: "Raspored",
+                newName: "RedniBrojPrijava");
+
+            migrationBuilder.RenameColumn(
+                name: "PrezimeTakmičara",
+                table: "Raspored",
+                newName: "PrezimePrijava");
+
+            migrationBuilder.RenameColumn(
+                name: "NacionalnostTakmičara",
+                table: "Raspored",
+                newName: "NacionalnostPrijava");
+
+            migrationBuilder.RenameColumn(
+                name: "Kategorija",
+                table: "Raspored",
+                newName: "KategorijaPrijava");
+
+            migrationBuilder.RenameColumn(
+                name: "ImeTakmičara",
+                table: "Raspored",
+                newName: "ImePrijava");
+
+            migrationBuilder.RenameColumn(
+                name: "Disciplina",
+                table: "Raspored",
+                newName: "DisciplinaPrijava");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StazaPrijava",
+                table: "Raspored",
+                newName: "Staza");
+
+            migrationBuilder.RenameColumn(
+                name: "RedniBrojPrijava",
+                table: "Raspored",
+                newName: "RedniBroj");
+
+            migrationBuilder.RenameColumn(
+                name: "PrezimePrijava",
+                table: "Raspored",
+                newName: "PrezimeTakmičara");
+
+            migrationBuilder.RenameColumn(
+                name: "NacionalnostPrijava",
+                table: "Raspored",
+                newName: "NacionalnostTakmičara");
+
+            migrationBuilder.RenameColumn(
+                name: "KategorijaPrijava",
+                table: "Raspored",
+                newName: "Kategorija");
+
+            migrationBuilder.RenameColumn(
+                name: "ImePrijava",
+                table: "Raspored",
+                newName: "ImeTakmičara");
+
+            migrationBuilder.RenameColumn(
+                name: "DisciplinaPrijava",
+                table: "Raspored",
+                newName: "Disciplina");
+        }
+    }
+}
